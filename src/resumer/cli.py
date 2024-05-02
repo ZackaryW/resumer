0caa0085d18086d7d831ed7227f5122cd697d9a2
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-05-02 10:35:14 -0700

    feat(cli): add list profiles

--- src/resumer/cli.py
+++ src/resumer/cli.py
@@ -3,12 +3,19 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from resumer.profiles import get_profile
+from resumer.profiles import get_profile, list_submodules
 
 @click.command()
 @click.option("--copy-temp-folder", "-ct", default=False, is_flag=True)
 @click.option("--profile", "-p", help="profile to use")
-def cli(copy_temp_folder, profile):
+@click.option("--list", "-l", default=False, is_flag=True, help="list available profiles")
+def cli(copy_temp_folder, profile, list):
+    if list:
+        for p in list_submodules():
+            click.echo(p)
+
+        return
+
     if not profile:
         click.echo("no profile specified")
         return
